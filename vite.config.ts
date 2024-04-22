f4b1a613a6005b35faa0abbf4aa7691cda2e1063
ReganHe <heoo547627@126.com>
2024-04-22 17:42:04 +0800

    AntDesignProVue3-Vite配置精简 AntDesignProVue3-Vite配置-插件提取

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig, loadEnv } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import path from 'path'
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
-import UnoCSS from 'unocss/vite'
-import vueSetupExtend from 'vite-plugin-vue-setup-extend'
-import { visualizer } from 'rollup-plugin-visualizer'
-
-const lifecycle = process.env.npm_lifecycle_event
+import { createVitePlugins } from './build/vite/plugin'
 
 export default defineConfig(({ command, mode }) => {
+  const isBuild = command === 'build'
   const root = process.cwd()
   const env = loadEnv(mode, root)
   console.log(env)
@@ -48,31 +41,13 @@ export default defineConfig(({ command, mode }) => {
       brotliSize: false,
       chunkSizeWarningLimit: 2000
     },
-    plugins: [
-      vue(),
-      vueJsx(),
-      UnoCSS(),
-      createSvgIconsPlugin({
-        // 指定需要缓存的图标文件夹
-        iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
-        // 指定symbolId格式
-        symbolId: 'icon-[dir]-[name]'
-      }),
-      // Components({
-      //   dts: true,
-      //   resolvers: [AntDesignVueResolver()],
-      //   include: [/\.vue$/, /\.tsx$/],
-      // }),
-      vueSetupExtend(),
-      lifecycle === 'report' ? (visualizer({ gzipSize: true, open: true, brotliSize: true, filename: 'report.html' }) as Plugin) : []
-    ],
     css: {
       preprocessorOptions: {
         less: {
-          // modifyVars: generateModifyVars(),
           javascriptEnabled: true
         }
       }
-    }
+    },
+    plugins: createVitePlugins(env, isBuild)
   }
 })
