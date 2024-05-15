d13dd10a3b8122a60b9259d480d890b1a15d1821
ReganHe <heoo547627@126.com>
2024-05-15 13:11:41 +0800

    Breadcrumb组件重构

--- src/components/Breadcrumb/index.vue
+++ src/components/Breadcrumb/index.vue
@@ -1,83 +1,34 @@
 <template>
   <div class="breadcrumb">
-    <!-- {{ breadcrumbData }} -->
     <a-breadcrumb>
-      <transition-group name="breadcrumb">
-        <a-breadcrumb-item v-for="(item, index) in breadcrumbData" :key="index">
-          <!-- 不可点击项 -->
-          <a-breadcrumb-item v-if="index === breadcrumbData.length - 1">
-            {{
-              item.meta.title
-            }}
-          </a-breadcrumb-item>
-          <!-- 可点击项 -->
-          <!-- <a-breadcrumb-item v-else> -->
-          <a v-else @click="onLinkClick(item)" :key="index">
-            {{ item.meta.title }}
-          </a>
-          <!-- </a-breadcrumb-item> -->
-        </a-breadcrumb-item>
-      </transition-group>
+      <a-breadcrumb-item v-for="(item, index) in breadcrumbData" :key="index">
+        <!-- 不可点击项 -->
+        <span v-if="index === breadcrumbData.length - 1">
+          {{
+            item.meta.title
+          }}
+        </span>
+        <!-- 可点击项 -->
+        <router-link v-else :to="item.path">
+          {{ item.meta.title }}
+        </router-link>
+      </a-breadcrumb-item>
     </a-breadcrumb>
   </div>
 </template>
 
 <script lang="ts" setup>
-// #region import
-//库
-import { ref, watch } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
-//#endregion
+import { computed, getCurrentInstance, ComponentInternalInstance } from 'vue'
 
-// #region 初始化
-const route = useRoute()
-// 生成数组数据
-const breadcrumbData = ref([] as any)
-const getBreadcrumbData = () => {
-  breadcrumbData.value = route.matched.filter(
+const { proxy } = getCurrentInstance() as ComponentInternalInstance
+const breadcrumbData = computed(() => {
+  return proxy!.$route.matched.filter(
     item => item.meta && item.meta.title
   )
-}
-// 监听路由变化时触发
-watch(
-  route,
-  () => {
-    getBreadcrumbData()
-  },
-  {
-    immediate: true
-  }
-)
-
-// 处理点击事件
-const router = useRouter()
-const onLinkClick = item => {
-  router.push(item.path)
-}
-
-// #endregion
+})
 </script>
 <style scoped lang="scss">
 .breadcrumb {
   display: inline-block;
-}
-
-.breadcrumb-enter-active {
-  transition: all 0.5s;
-
-}
-
-.breadcrumb-leave-active {
-  transition: all 0.01s;
-}
-
-.breadcrumb-enter-from,
-.breadcrumb-leave-active {
-  opacity: 0;
-  transform: translateX(20px);
-}
-
-.breadcrumb-leave-active {
-  position: absolute;
 }
 </style>
