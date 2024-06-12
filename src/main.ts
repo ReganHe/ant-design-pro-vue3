83a0d97daee677075e8a8917b3a13cb6ad86b9ae
ReganHe <heoo547627@126.com>
2024-06-12 22:23:19 +0800

    Vue3模板库-Vue组件引入方式优化

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,8 @@
 import { createApp } from 'vue'
 import Antd from 'ant-design-vue'
-// import Castor3UI from '@neweratech/castor3-ui'
+import Castor3UI from '@neweratech/castor3-ui'
 import setupDefaultSetting from '@/utils/default-setting'
 import SvgIcon from '@/components/SvgIcon/index.vue'
-// import { CaCommonQuery } from '@/components/CaCommonQuery'
-// import { CaCommonTable } from '@/components/CaCommonTable'
-// import { CaCommonForm } from '@/components/CaCommonForm'
-import CaCommonQuery from '@neweratech/castor3-ui/es/CaCommonQuery'
-import CaCommonTable from '@neweratech/castor3-ui/es/CaCommonTable'
-import CaCommonForm from '@neweratech/castor3-ui/es/CaCommonForm'
 import App from '@/App.vue'
 import { router } from '@/router'
 import { store } from '@/store'
@@ -20,13 +14,10 @@ import '@/utils/passive-events'
 
 const app = createApp(App)
 app.use(Antd)
-// app.use(Castor3UI)
+app.use(Castor3UI)
 app.use(router)
 app.use(store)
 app.component('SvgIcon', SvgIcon)
-app.component('CaCommonQuery', CaCommonQuery)
-app.component('CaCommonTable', CaCommonTable)
-app.component('CaCommonForm', CaCommonForm)
 
 /** 挂载全局变量 dayjs */
 app.config.globalProperties.$dayjs = dayjsWrapper
